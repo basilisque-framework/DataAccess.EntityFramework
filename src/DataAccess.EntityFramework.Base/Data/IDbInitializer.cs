e550f198a332cc5a3514a3d138d282006e07ca40
Alexander Stärk <alexander.staerk@asprog.de>
2025-11-22 08:43:14 +0100

    #1 - added copyright headers

--- src/DataAccess.EntityFramework.Base/Data/IDbInitializer.cs
+++ src/DataAccess.EntityFramework.Base/Data/IDbInitializer.cs
@@ -1,4 +1,20 @@
-﻿namespace Basilisque.DataAccess.EntityFramework.Base.Data;
+﻿/*
+   Copyright 2025 Alexander Stärk
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+namespace Basilisque.DataAccess.EntityFramework.Base.Data;
 
 /// <summary>
 /// This interface is being used to find and execute all initializers; it should be implemented by all DbInitializers of the application.
