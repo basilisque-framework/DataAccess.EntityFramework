3a8119c3c5e968eb1128a42c859d54cc276ddd73
Alexander Stärk <alexander.staerk@asprog.de>
2026-09-11 09:47:58 +0200

    #1 - added test for DbContext inheritance

---
+++ src/DataAccess.EntityFramework.CodeAnalysis.Unit.Tests/Generators/DesignTimeDbContextFactoryGenerator/Generate_1_DbContext_1_Multiple_Inheritance_Levels.cs
@@ -0,0 +1,106 @@
+﻿/*
+   Copyright 2026 Alexander Stärk
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using Basilisque.DataAccess.EntityFramework.CodeAnalysis.Generators;
+using Microsoft.CodeAnalysis;
+
+namespace Basilisque.DataAccess.EntityFramework.CodeAnalysis.Unit.Tests.Generators.DesignTimeDbContextFactoryGenerator;
+
+[InheritsTests]
+[Category(DesignTimeDbContextFactoryGeneratorCategory)]
+public class Generate_1_DbContext_1_Multiple_Inheritance_Levels : BaseDataAccessEntityFrameworkGeneratorTest
+{
+    protected override IEnumerable<(string Name, string SourceText)> GetExpectedDbContextFactorySources()
+    {
+        yield return (
+                Name: "DbContextAssembly.DataAccess.EFG.TestObjects.Generate_1_DbContext.MyDbContextDesignTimeFactory.g.cs",
+                SourceText: @$"{CommonGeneratorData.GeneratedFileSharedHeaderWithNullable}
+namespace DbContextAssembly.DataAccess.EFG.TestObjects.Generate_1_DbContext;
+
+/// <inheritdoc />
+{CommonGeneratorData.GeneratedClassSharedAttributesNotIndented}
+public partial class MyDbContextDesignTimeFactory : global::Basilisque.DataAccess.EntityFramework.SqlServer.Design.SqlServerBaseDesignTimeDbContextFactory<DbContextAssembly.DataAccess.EFG.TestObjects.Generate_1_DbContext.MyDbContext>
+{{
+    /// <inheritdoc />
+    protected sealed override void ConfigureMigrationAssemblyProvider(global::Microsoft.Extensions.DependencyInjection.IServiceCollection services, string[] args)
+    {{
+        global::Microsoft.Extensions.DependencyInjection.ServiceCollectionServiceExtensions.AddTransient<global::Basilisque.DataAccess.EntityFramework.Base.Model.IMigrationAssemblyProvider, global::Basilisque.DataAccess.EntityFramework.CodeAnalysis.Unit.Tests.Generated.MigrationAssemblyProvider>(services);
+    }}
+}}
+
+#nullable restore");
+    }
+
+    protected override IEnumerable<(string AssemblyName, IReadOnlyList<string> Sources, IReadOnlyList<MetadataReference> DbContextAssemblyReferences)> GetDbContextsUnderTest()
+    {
+        yield return ("DbContextAssembly", [
+            @"
+#nullable enable
+
+namespace DbContextAssembly.DataAccess.EFG.TestObjects.Base;
+
+/// <inheritdoc />
+public abstract class CustomBaseDbContext<TDbContext> : Basilisque.DataAccess.EntityFramework.Relational.Model.BaseDbContext<TDbContext>
+    where TDbContext : CustomBaseDbContext<TDbContext>
+{
+    /// <inheritdoc />
+    public CustomBaseDbContext(
+        Basilisque.DataAccess.EntityFramework.Base.DependencyInjection.IDbProviderServiceProvider dbProviderServiceProvider,
+        [System.Runtime.CompilerServices.CallerFilePath] string dbContextSourceFilePath = """"
+        )
+        : base(dbProviderServiceProvider, dbContextSourceFilePath)
+    { }
+}
+",
+            @"
+#nullable enable
+
+using DbContextAssembly.DataAccess.EFG.TestObjects.Base;
+
+namespace DbContextAssembly.DataAccess.EFG.TestObjects.Generate_1_DbContext;
+
+/// <summary>
+/// Test DbContext
+/// </summary>
+public class MyDbContext : CustomBaseDbContext<MyDbContext>
+{
+    /// <summary>
+    /// Creates a new MyDbContext.
+    /// </summary>
+    public MyDbContext(Basilisque.DataAccess.EntityFramework.Base.DependencyInjection.IDbProviderServiceProvider dbProviderServiceProvider)
+        : base(dbProviderServiceProvider)
+    { }
+
+    /// <inheritdoc />
+    protected override string GetAppAreaPrefix()
+    {
+        return ""MyAppArea"";
+    }
+}
+"], []);
+    }
+
+    protected override IEnumerable<string> GetExpectedDbContextDependencyInjectionRegistrations()
+    {
+        yield return "DbContextAssembly.DataAccess.EFG.TestObjects.Generate_1_DbContext.MyDbContext";
+    }
+
+    protected override IEnumerable<MetadataReference> GetMigrationAssemblyReferences()
+    {
+        yield return ReferenceAssembly_SqlServerBaseDesignTimeDbContextFactory;
+    }
+}
+
